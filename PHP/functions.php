4b9d314dc8b860970617ed595f3f32dcb034dfab
FelipeMartinsFrudeli <felipem.frudeli.dev@gmail.com>
2024-03-12 08:01:11 -0300

    php branch added

---
+++ PHP/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function soma($a, $b) {
+    echo $a + $b;
+}
+
+// Pascal Case
+function SubtrairNumeros($a, $b) {
+    echo $a - $b;
+}
+
+// Snake Case
+function multiplicar_numeros($a, $b) {
+    echo $a * $b;
+}
+
+echo soma(1, 2)."<br>";
+echo SubtrairNumeros(1, 2)."<br>";
+echo multiplicar_numeros(1, 2)."<br>";
+
+?>
