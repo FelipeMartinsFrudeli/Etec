cf92a8c28fba181f734fde148262d71b49eb0d44
FelipeMartinsFrudeli <felipem.frudeli.dev@gmail.com>
2024-03-12 10:38:55 -0300

    functions with php

--- PHP/functions.php
+++ PHP/functions.php
@@ -14,8 +14,27 @@ function multiplicar_numeros($a, $b) {
     echo $a * $b;
 }
 
+// Camel Case
+function dividirNumero($a, $b) {
+    echo $a / $b;
+}
+
+function recepicionarUsuario($nome = "User") {
+    echo "Ola $nome <br>";
+}
+
+function saudarPessoa($nome = null) {
+    echo "Oi $nome <br>";
+}
+
 echo soma(1, 2)."<br>";
 echo SubtrairNumeros(1, 2)."<br>";
 echo multiplicar_numeros(1, 2)."<br>";
+echo dividirNumero(1, 2)."<br>";
 
+recepicionarUsuario();
+saudarPessoa();
+saudarPessoa('user2');
 ?>
+
+
